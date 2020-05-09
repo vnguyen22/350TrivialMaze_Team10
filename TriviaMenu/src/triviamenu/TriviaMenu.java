d6e42584b1827e3e6e1ca456734a12cb2f512bcd
Graham Mix <gmix@eagles.ewu.edu>
2020-05-09 00:55:20 -0700

    Updated menu

--- TriviaMenu/src/triviamenu/TriviaMenu.java
+++ TriviaMenu/src/triviamenu/TriviaMenu.java
@@ -6,11 +6,13 @@
 package triviamenu;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.NodeOrientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,31 +23,61 @@ public class TriviaMenu extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        GridPane root = constructGrid();
+        Scene scene = new Scene(root, root.getWidth(), root.getHeight());
+        scene.setNodeOrientation(NodeOrientation.INHERIT);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Trivia Maze");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    private Button constructButton(String text){
+        Button butt = new Button();
+        butt.setPrefWidth(400);
+        butt.setPrefHeight(200);
+        butt.setText(text);
+        return butt;
+    }
+    
+    private GridPane constructGrid(){
+        GridPane root = new GridPane();
+        Button newGameButton = constructButton("Start a new game");
+        newGameButton.setOnAction(actionEvent -> onNew());
+        root.add(newGameButton, 0, 0);
+        Button loadGameButton = constructButton("Load a previous game");
+        loadGameButton.setOnAction(actionEvent -> onLoad());
+        root.add(loadGameButton, 0, 1);
+        Button questButton = constructButton("Add your own question");
+        questButton.setOnAction(actionEvent -> onQuest());
+        root.add(questButton, 0, 2);
+        Button quitButton = constructButton("Quit");
+        quitButton.setOnAction(actionEvent -> onQuit());
+        root.add(quitButton, 0, 3);
+        return root;
+    }
+    
+    
+    private int onNew(){
+        return 0;
+    }
+    
+    private int onLoad(){
+        return 0;
+    }
+    
+    private int onQuest(){
+        return 0;
+    }
+    
+    private void onQuit(){
+        Platform.exit();
     }
     
 }
