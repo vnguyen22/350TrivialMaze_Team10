2dc83100c7210ceb33f6c88d84cef6f48783941a
Graham Mix <gmix@eagles.ewu.edu>
2020-05-22 21:41:48 -0700

    Created a PlayerData class based off Van's work

---
+++ TriviaMenu/src/triviamenu/PlayerData.java
@@ -0,0 +1,24 @@
+package triviamenu;
+
+public class MazeSettings implements Serializable{
+    private int playerPosX, playerPosY, mazeNumRow, mazeNumCol;
+    private static MazeSettings mazeSettings;
+
+    private MazeSettings(int x, int y, int row, int col){
+        this.playerPosX = x;
+        this.playerPosY = y;
+        this.mazeNumRow = row;
+        this.mazeNumCol = col;
+    }
+
+    public static MazeSettings createSettings(int x, int y, int row, int col){
+        if (mazeSettings == null){
+            mazeSettings = new MazeSettings(x, y, row, col);
+            return mazeSettings;
+        }
+        else {
+            return mazeSettings;
+        }
+    }
+
+}
